5e4215fbecf5cfd58d048e5de51ce3d6bfe9fdde
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2021-07-03 19:18:41 +0530

    Implemented Tabbed view for Work Experience

--- index.js
+++ index.js
@@ -17,3 +17,22 @@ function setInitialView(){
 }
 
 setInitialView();
+
+
+// Tabs
+function openTab(evt, tabName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
